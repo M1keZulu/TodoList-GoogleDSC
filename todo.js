30c8c6446ec5a155237f0313bd7eb93e0338a461
Muhammad Zain Ul Abiddin <afsarhussain9@hotmail.com>
2020-12-05 14:21:48 +0500

    Update 1

---
+++ todo.js
@@ -0,0 +1,10 @@
+var input_list = document.getElementById('newTask');
+var mylist = document.getElementById('in_list');
+
+function add(){
+    mylist.insertAdjacentHTML('beforeend', '<li>' + input_list.value + '<span class="click" onclick="remove(this)">X</span> </li>');
+}
+
+function remove(task){
+    task.parentElement.style.display = "none";
+}
