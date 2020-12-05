20e1d322f10e20befd395b0dbea0cdfb1bb2e51e
Muhammad Zain Ul Abiddin <afsarhussain9@hotmail.com>
2020-12-05 22:49:15 +0500

    Basic Functionality Update

--- todo.js
+++ todo.js
@@ -2,7 +2,7 @@ var input_list = document.getElementById('newTask');
 var mylist = document.getElementById('in_list');
 
 function add(){
-    mylist.insertAdjacentHTML('beforeend', '<li>' + input_list.value + '<span class="click" onclick="remove(this)">X</span> </li>');
+    mylist.insertAdjacentHTML('beforeend', '<li class="fade-in">' + input_list.value + '<span class="click" onclick="remove(this)">X</span> </li>');
 }
 
 function remove(task){
